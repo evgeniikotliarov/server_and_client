59ed0afb2659aaace48f7dfe6c7a96a59d5fa6cc
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-04-24 23:37:29 +0600

    Иннициализация классов

---
+++ server/Publication.py
@@ -0,0 +1,5 @@
+class Publication:
+    def __init__(self, title, text, attachments):
+        self.title = title
+        self.text = text
+        self.attachments = attachments
---
+++ server/User.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, name, password, session, publication):
+        self.name = name
+        self.password = password
+        self.session = session
+        self.publication = publication
