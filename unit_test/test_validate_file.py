9dfd34619ac1f5cabd817e344798107dc8a5b9e6
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-04-29 23:57:14 +0600

    Написал тест на validate_file

---
+++ unit_test/test_validate_file.py
@@ -0,0 +1,43 @@
+import os.path
+import unittest
+
+from definitions import PUBLIC_FOLDER
+from server.router.path_validator import *
+
+
+class TestValidateFile(unittest.TestCase):
+    def test_is_directory_allowed(self):
+        file = 'styles/style.css'
+        result = is_allowed_directory(strip_slash(file))
+        self.assertTrue(result)
+
+    def test_directory_is_not_allowed(self):
+        file = '../../index.html'
+        result = is_allowed_directory(strip_slash(file))
+        self.assertFalse(result)
+
+    def test_file_exists(self):
+        file = '/index.html'
+        result = file_exists(get_public(strip_slash(file)))
+        self.assertTrue(result)
+
+    def test_file_not_exists(self):
+        file = '/end.html'
+        result = file_exists(get_public(strip_slash(file)))
+        self.assertFalse(result)
+
+    def test_get_file(self):
+        file = '/index.html'
+        file_path = get_public(strip_slash(file))
+        result = get_file(file)
+        self.assertEqual(result, file_path)
+
+
+def get_public(file_name):
+    return os.path.join(PUBLIC_FOLDER, file_name)
+
+def strip_slash(file_name):
+    return file_name.strip('/')
+
+if __name__ == '__main__':
+    unittest.main()
