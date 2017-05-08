898a386555e5703ff2f7bca0d3c7533e891f44b3
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-05-09 00:30:55 +0600

    Модифицировал string

--- util/strings.py
+++ util/strings.py
@@ -1,6 +1,13 @@
-def bytes_to_strings(*byte_strings):
-    for byte_str in byte_strings:
+def ensure_string(*byte_strings):
+
+    def make_string(byte_str):
         if isinstance(byte_str, bytes):
             yield byte_str.decode()
         else:
             yield str(byte_str)
+
+    if len(byte_strings) == 1:
+        return make_string(byte_strings[0])
+
+    for byte_str in byte_strings:
+        yield make_string(byte_str)
