eab460519b497c9cd5e88e8cbe5ea0bb4d188de1
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-05-08 22:59:34 +0600

    Обновил .gitignore добавил картинки

---
+++ util/strings.py
@@ -0,0 +1,9 @@
+def bytes_to_strings(*byte_strings):
+    for byte_str in byte_strings:
+        if isinstance(byte_str, bytes):
+            yield byte_str.decode()
+        else:
+            yield str(byte_str)
+
+a,b = bytes_to_strings(b'A','B')
+print(b'A')
