e354e5cded5e0ded30ba5e671b4a15b44d1c532d
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-05-09 23:18:16 +0600

    Authentification

--- server/actions/auth.py
+++ server/actions/auth.py
@@ -1,18 +1,25 @@
 from storage.users import UsersDAO
 from server.session import *
 
+from server.form_encodigs.url_encoder import *
+import server.response.response_builder as set_cookie
+
 
 def do_auth(request, response_builder):
-    print("Requested auth")
+    a_user = parse(request.body)
+    username = a_user['user']
+    password = a_user['password']
+    valid_user = _validate_user(username, password)
+    if valid_user:
+        response_builder.set_cookie("session=%s" % id)
 
-def _validate_user(self):
+
+def _validate_user(username, password):
     try:
-        UsersDAO.validate_user(self.username, self.password)
-        self.current_user = UsersDAO.get_user(self.username)
+        UsersDAO.validate_user(username, password)
+        current_user = UsersDAO.get_user(username)
         return True
     except ValueError:
         return False
 
-def set_cookie(response_builder):
-    pass
 
