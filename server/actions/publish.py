971708540d79bf90389e5f948636564813d8c498
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-05-15 22:50:00 +0600

    Переименовал Регистрацию

--- server/actions/publish.py
+++ server/actions/publish.py
@@ -1,7 +1,9 @@
 from storage.users import *
 from server.form_encodings.url_encoded import parse_url_encoded
 from storage.users import UsersDAO
+from util.redirect_generator import redirect_builder
 import util.constants.response_codes as codes
+from util.constants.paths import INDEX_PAGE
 
 def do_register(request, response_builder):
     data = request.body
@@ -14,5 +16,7 @@ def do_register(request, response_builder):
     created_code, created_message = codes.CREATED
     response_builder.set_code(created_code)
     response_builder.set_message(created_message)
+    redirect = redirect_builder(INDEX_PAGE)
+    response_builder.set_redirect(redirect)
     return response_builder
 
