e8a39067f081763bc8af204b77ca1267e3b904a2
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-05-29 23:36:09 +0600

    Added factory

---
+++ storage/publications_factory.py
@@ -0,0 +1,12 @@
+from util.constants.const_main import IN_MEMORY, DATABASE
+from storage.DAO.publications import Publications
+from storage.data_storages.memory import publications_storage
+
+
+class PublicationDAOFactory:
+    @staticmethod
+    def get_storage(storage_type):
+        if storage_type == IN_MEMORY:
+            return Publications(publications_storage)
+        if storage_type == DATABASE:
+            return None #TODO to be added
