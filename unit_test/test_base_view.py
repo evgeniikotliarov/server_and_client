3e3d95b58d582745c8a1dec47bbb450f1000d488
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-06-04 23:23:13 +0600

    Unittest deleted

--- unit_test/test_base_view.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from template.base_view import *
-
-class TestBaseView(unittest.TestCase):
-    def test_insert(self):
-        test_str = "My test content"
-        view = insert_content(test_str)
-        self.assertTrue(test_str in view)
